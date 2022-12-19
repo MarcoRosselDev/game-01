6d7925ba3417fc2fca79f50028c1bbc5be69de06
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-12-19 12:52:11 -0300

    class 3.5

--- index.js
+++ index.js
@@ -6,27 +6,54 @@ const CANVAS_HEIGHT = (canvas.height = 600);
 
 const playerImage = new Image();
 playerImage.src = "shadow_dog.png";
-const spriteWithe = 575;
+const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 4;
 let gameFrame = 0;
 const staggerFrame = 5;
+const spriteAnimations = [];
+
+const animationStates = [
+  {
+    name: "idle",
+    frames: 7,
+  },
+  {
+    name: "jump",
+    frames: 7,
+  },
+];
+
+animationStates.forEach((state, index) => {
+  let frames = {
+    loc: [],
+  };
+
+  for (let j = 0; j < state.frames; j++) {
+    let positionX = j * spriteWidth;
+    let positionY = index * spriteHeight;
+    frames.loc.push({ x: positionX, y: positionY });
+  }
+  spriteAnimations[state.name] = frames;
+});
+
+console.log(animationStates);
 
 function animate() {
   ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-  let position = Math.floor(gameFrame / staggerFrame) % 10;
-  frameX = spriteWithe * position;
+  let position =
+    Math.floor(gameFrame / staggerFrame) % spriteAnimations["idle"].loc.length;
+  let frameX = spriteWidth * position;
+  let frameY = spriteAnimations["idle"].loc[position].y;
 
   ctx.drawImage(
     playerImage,
     frameX,
     frameY * spriteHeight,
-    spriteWithe,
+    spriteWidth,
     spriteHeight,
     0,
     0,
-    spriteWithe,
+    spriteWidth,
     spriteHeight
   );
 
